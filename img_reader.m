11238316a306664a61d1a3e34dce83c5f4bb312a
omsmarian <omsmarian@gmail.com>
2026-01-23 00:12:03 -0300

    detectar verdes

--- img_reader.m
+++ img_reader.m
@@ -1,67 +1,21 @@
 % Cargo la imagen
 img = iread("img\test1.png", 'double');
 
-% Mostrar imagen original
-figure;
-subplot(1,2,1);
-idisp(img);
-title('Imagen original');
+%img_gamma = igamm(img, 0.45);
+R = idouble(img(:,:,1));
+G = idouble(img(:,:,2));
+B = idouble(img(:,:,3));
+mask_G = G > 0.5;
+mask_R = R < 0.5;
 
-% Histograma de imagen original
-subplot(1,2,2);
-ihist(img);
-title('Histograma original');
+diff_im = imsubtract(G, rgb2gray(img));
+%diff_im = im2bw(diff_im,0.18);
+%diff_im = bwareaopen(diff_im, ceil(300));
+green = cat(3, zeros(size(R)), mask_G, zeros(size(B)));
+red = cat(3, mask_R, zeros(size(G)), zeros(size(B)));
 
-stop=input('continuar?');
-close all
+green2 = inormhist(imono(green));
+red2 = inormhist(imono(red));
+diff_im = imsubtract(green2, red2);
 
-% Corrección gamma
-img = igamm(img, 0.9);
-
-% Mostrar imagen con gamma corregido
-figure;
-subplot(1,2,1);
-idisp(img);
-title('Imagen con gamma corregido');
-
-% Histograma después de gamma
-subplot(1,2,2);
-ihist(img);
-title('Histograma después de gamma');
-
-stop=input('continuar?');
-close all
-
-% Pasar a escala de grises
-img_grey = imono(img);
-img_grey = ihist(img_grey);
-
-
-% Normalizar histograma
-grey_normal = inormhist(img_grey);
-
-% Mostrar imagen normalizada
-figure;
-subplot(1,2,1);
-idisp(img_grey);
-title('Imagen normalizada');
-
-% Histograma normalizado
-subplot(1,2,2);
-ihist(grey_normal);
-title('Histograma normalizado');
-
-stop=input('continuar?');
-close all
-
-
-bordes = icanny(img);
-
-idisp(bordes);
-stop=input('continuar?');
-close all
-% Buscar los verdes en base a un threshold
-
-
-
-% Tenemos q usar Hough para los bordes
+idisp(green);
